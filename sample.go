97b734b4cd188a41b002a71b1c66e88e2e407078
Shogo Tsutsumi <shogo-tsutsumi@retty.me>
2022-03-16 15:16:01 +0900

    implement fizzbuzz

--- sample.go
+++ sample.go
@@ -1,9 +1,11 @@
 package tddbc
 
+import "fmt"
+
 func Say(greeting string) string {
 	return greeting + " TDD BootCamp!!"
 }
 
 func FizzBuzz(value int) string {
-	return ""
+	return fmt.Sprintf("%d", value)
 }
